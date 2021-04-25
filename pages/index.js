030cabdc5d6e872e99475a210e0d0bdd0d31bce8
vishy-dev <agarwal.vrishank@gmail.com>
2021-04-25 20:23:20 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,51 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faDiscord,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
+  const [notifVisible, setNotifVisible] = useState(false);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div
+      className={`flex-center-col bg-[#1f2124] w-screen h-screen text-center`}
+    >
+      <h1
+        className={`text-8xl font-bold text-gradient bg-gradient-to-r from-blue-500 to-indigo-700 font-mono tracking-wider`}
+      >
+        f1sh
+      </h1>
+      <div className={`text-gray-300 m-4 max-w-xl font-mono`}>
+        👋&nbsp;I like coding things, and I also have interest in
+        games, such as <a href="https://minecraft.com">Minecraft</a>{' '}
+        and <a href="https://beatsaber.com">Beat Saber</a>!
+      </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      <div
+        className={`flex-row color-current text-gray-200 text-3xl`}
+      >
+        <FontAwesomeIcon
+          icon={faDiscord}
+          className={`mr-4 hover-trans`}
+          onClick={() => {
+            navigator.clipboard.writeText('F1shNotFound#5117');
+            setNotifVisible(true);
+            setTimeout(() => setNotifVisible(false), 5000);
+          }}
+        />
+        <a href="https://twitter.com/vishyfishy2">
+          <FontAwesomeIcon icon={faTwitter} />
         </a>
-      </footer>
+      </div>
+
+      <div
+        className={`fixed bottom-0 right-0 bg-blue-400 text-white rounded-full px-4 py-2 m-4 transition-opacity ease-in duration-200 ${
+          !notifVisible ? 'opacity-0' : null
+        }`}
+      >
+        Copied to Clipboard!
+      </div>
     </div>
-  )
+  );
 }
