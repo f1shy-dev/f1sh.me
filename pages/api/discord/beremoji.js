e25c7407bfe76536c3f7a3b454cbe6c62c7a2d0a
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-08 14:09:20 +0100

    feat: beremoji endpoint

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+  if (req.method === 'POST') return postHandler(req, res);
+  res.status(400);
 }
+
+const postHandler = (req, res) => {
+  if (!req.body) return res.status(400);
+  if (req.body.type === 1) return res.status(200).json({ type: 1 });
+  res.status(200).json({ name: 'John Doe' });
+};
