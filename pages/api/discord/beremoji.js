76c3169b277b7f43dfe652091f858a1dc1529c46
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-08 16:53:21 +0100

    feat: logging

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -2,13 +2,19 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
 
+  console.log(req);
+
   if (req.method === 'POST') return postHandler(req, res);
+  console.log('GET success');
   res.status(200).json({ status: 'success' });
 }
 
 const postHandler = (req, res) => {
+  console.log('notype');
   if (!req.body.type) return res.status(400);
+  console.log('PING');
   if (req.body.type === '1') return res.status(200).json({ type: 1 });
+  console.log('command');
   res.status(200).json({
     type: 4,
     data: {
