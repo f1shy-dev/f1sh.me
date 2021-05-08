5c1a165bcec43f72381a636c3e719cfaf4796d7a
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-08 16:58:54 +0100

    feat: screw you typed crap

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -2,24 +2,19 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  console.log(req);
-
   if (req.method === 'POST') return postHandler(req, res);
-  console.log('GET success');
   res.status(200).json({ status: 'success' });
 }
 
 const postHandler = (req, res) => {
-  console.log('notype');
   if (!req.body.type) return res.status(400);
-  console.log('PING');
-  if (req.body.type === '1') return res.status(200).json({ type: 1 });
-  console.log('command');
+  if (req.body.type === 1) return res.status(200).json({ type: 1 });
+
   res.status(200).json({
     type: 4,
     data: {
       tts: false,
-      content: 'Congrats on sending your command!',
+      content: 'If you see this, something worked!',
       embeds: [],
       allowed_mentions: { parse: [] },
     },
