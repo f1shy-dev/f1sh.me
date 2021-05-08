f593564080988bf03f40c63ee754981d4cd35548
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-08 17:23:31 +0100

    feat: sign verification

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -1,6 +1,19 @@
+import nacl from 'tweetnacl';
+
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(req.headers['X-Signature-Timestamp'] + req.body),
+    Buffer.from(req.headers['X-Signature-Ed25519'], 'hex'),
+    Buffer.from(process.env.DISCORD_PUB_KEY, 'hex'),
+  );
+
+  if (!isVerified)
+    return res
+      .status(401)
+      .json({ status: 'error', error: 'invalid request signature' });
 
   if (req.method === 'POST') return postHandler(req, res);
   res.status(200).json({ status: 'success' });
