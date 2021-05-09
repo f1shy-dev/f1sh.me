e9ef4d7a796ce9f89cad5a5db6f5b37a53af2c1d
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-09 15:03:58 +0100

    handler system

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -1,53 +1,36 @@
-import {
-  enableCORS,
-  verifyDiscordSignature,
-  errorResponse,
-  pingResponse,
-} from '../../../lib/discordTools';
 import { convertFuzzyFeeling } from 'beremoji';
+import discordCommandHandler from '../../../lib/discordCommandHandler';
 
 export default function handler(req, res) {
-  enableCORS(res);
-  if (!verifyDiscordSignature(req))
-    return errorResponse(res, 'Invalid Signature');
-
-  if (req.method === 'POST') return postHandler(req, res);
-  errorResponse(res, 'Invalid Request Method');
-}
-
-const postHandler = (req, res) => {
-  if (!req.body.type) return errorResponse(res, 'Invalid Form Body');
-  if (req.body.type === 1) return pingResponse();
-  try {
+  const reqHandler = (req, res) => {
     const result = convertFuzzyFeeling(
       req.body.data.options[0].value,
     );
 
-    if (!result) throw Error('no result');
+    if (!result) throw Error('BeremojiNotFound');
 
-    return res.status(200).json({
-      type: 4,
-      data: {
-        tts: false,
-        content: result,
-        embeds: [],
-        allowed_mentions: { parse: [] },
-      },
-    });
-  } catch {
-    return res.status(200).json({
-      type: 4,
-      data: {
-        tts: false,
-        embeds: [
-          {
-            title: 'Error',
-            description: `The Beremoji \`${req.body.data.options[0].value}\` doesn't exist, or there was an error converting it to an emoji!`,
-            color: 15213861,
-          },
-        ],
-        allowed_mentions: { parse: [] },
-      },
-    });
-  }
-};
+    return {
+      tts: false,
+      content: result,
+      embeds: [],
+      allowed_mentions: { parse: [] },
+    };
+  };
+
+  const errorHandler = (req, res, err) => {
+    return {
+      tts: false,
+      content: '',
+      embeds: [
+        {
+          title: 'Error',
+          description: `The Beremoji \`${req.body.data.options[0].value}\` doesn't exist, or there was an error converting it to an emoji!`,
+          color: 15213861,
+        },
+      ],
+      allowed_mentions: { parse: [] },
+    };
+  };
+
+  return discordCommandHandler(req, res, reqHandler, errorHandler);
+}
