33f28d8fd01aed1f0807bbf817e0bb323e625b87
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-09 14:28:29 +0100

    maybe please maybe

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -18,14 +18,13 @@ export default function handler(req, res) {
 const postHandler = (req, res) => {
   if (!req.body.type) return errorResponse(res, 'Invalid Form Body');
   if (req.body.type === 1) return pingResponse();
-  console.log([
-    req.body.data,
-    req.body.data.options,
-    req.body.data.options[0],
-    convertFuzzyFeeling(req.body.data.options[0].value),
-  ]);
-  const result = convertFuzzyFeeling(req.body.data.options[0].value);
-  if (result)
+  try {
+    const result = convertFuzzyFeeling(
+      req.body.data.options[0].value,
+    );
+
+    if (!result) throw Error('no result');
+
     return res.status(200).json({
       type: 4,
       data: {
@@ -35,20 +34,20 @@ const postHandler = (req, res) => {
         allowed_mentions: { parse: [] },
       },
     });
-
-  return res.status(200).json({
-    type: 4,
-    data: {
-      tts: false,
-      content: result,
-      embeds: [
-        {
-          title: 'Error',
-          description: `The beremoji ${req.body.data.options[0].value} doesn't exist!`,
-          color: 15213861,
-        },
-      ],
-      allowed_mentions: { parse: [] },
-    },
-  });
+  } catch {
+    return res.status(200).json({
+      type: 4,
+      data: {
+        tts: false,
+        embeds: [
+          {
+            title: 'Error',
+            description: `The beremoji ${req.body.data.options[0].value} doesn't exist!`,
+            color: 15213861,
+          },
+        ],
+        allowed_mentions: { parse: [] },
+      },
+    });
+  }
 };
