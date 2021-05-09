dbd6511d6e71794d50f8a6e274755bfada89f796
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-09 14:18:33 +0100

    refactor

--- pages/api/discord/beremoji.js
+++ pages/api/discord/beremoji.js
@@ -1,46 +1,47 @@
-import nacl from 'tweetnacl';
+import {
+  enableCORS,
+  verifyDiscordSignature,
+  errorResponse,
+  pingResponse,
+} from '../../../lib/discordTools';
 import { convertFuzzyFeeling, convertExactFeeling } from 'beremoji';
 
 export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  const timestamp = req.headers['x-signature-timestamp'];
-  const signature = req.headers['x-signature-ed25519'];
-
-  if (!timestamp || !signature || !req.body)
-    return res.status(401).send('invalid request signature');
-
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp + JSON.stringify(req.body)),
-    Buffer.from(signature, 'hex'),
-    Buffer.from(process.env.DISCORD_PUB_KEY, 'hex'),
-  );
-
-  if (!isVerified)
-    return res.status(401).send('invalid request signature');
+  enableCORS(res);
+  if (!verifyDiscordSignature(req))
+    return errorResponse(res, 'Invalid Signature');
 
   if (req.method === 'POST') return postHandler(req, res);
-  res.status(200).json({ status: 'success' });
+  errorResponse(res, 'Invalid Request Method');
 }
 
 const postHandler = (req, res) => {
-  if (!req.body.type) return res.status(400);
-  if (req.body.type === 1) return res.status(200).json({ type: '1' });
-  let result = '';
-
-  if (req.body.data.name === 'beremoji')
-    result = convertExactFeeling(req.body.data.options[0].value);
-
-  if (req.body.data.name === 'feeling')
-    result = convertFuzzyFeeling(req.body.data.options[0].value);
+  if (!req.body.type) return errorResponse(res, 'Invalid Form Body');
+  if (req.body.type === 1) return pingResponse();
+  const result = convertFuzzyFeeling(req.body.data.options[0].value);
+  if (result)
+    return res.status(200).json({
+      type: 4,
+      data: {
+        tts: false,
+        content: result,
+        embeds: [],
+        allowed_mentions: { parse: [] },
+      },
+    });
 
   return res.status(200).json({
     type: 4,
     data: {
       tts: false,
-      content: result || "Couldn't find that beremoji!",
-      embeds: [],
+      content: result,
+      embeds: [
+        {
+          title: 'Error',
+          description: `The beremoji ${req.body.data.options[0].value} doesn't exist!`,
+          color: 15213861,
+        },
+      ],
       allowed_mentions: { parse: [] },
     },
   });
