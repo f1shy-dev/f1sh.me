a0016775c09e1ec7d5608343b0a6b099b2c64c76
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-11 22:01:10 +0000

    add redirect to 'cdn' hosted on github for random static files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/(cdn|dl|download|file|files)/:file*",
+        destination:
+          "https://github.com/f1shy-dev/random-cdn/raw/main/:file*",
+        permanent: false,
+      },
+    ];
+  },
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
